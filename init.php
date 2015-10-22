1dec2a3b8e5cbb5d8febcf53f77c51a936c80e09
Hamid Samak <hamidreza.samak@gmail.com>
2015-10-22 14:30:33 +0330

    Added comments

--- init.php
+++ init.php
@@ -25,14 +25,14 @@ if (file_exists(PATH . DS . 'security.php'))
 // include database connection object
 require_once PATH . DS . 'incs' . DS . 'database.class.php';
 
-// making connection to database
+// make connection to database
 $db = new Database(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME);
 
 // display error if install directory exists
 if (file_exists(PATH . DS . 'install') && is_dir(PATH . DS . 'install'))
 	die('Limny error: Please delete <em>install</em> directory.');
 
-// including Limny main functions
+// include Limny main functions
 require_once PATH . DS . 'incs' . DS . 'functions.php';
 
 // set Limny error handler as default error handling function

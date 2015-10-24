b670928625d4c7c968c4be76bfa647b307c98b9c
Hamid Samak <hamidreza.samak@gmail.com>
2015-10-24 11:49:21 +0330

    Added header and comments

--- incs/config.class.php
+++ incs/config.class.php
@@ -1,8 +1,20 @@
 <?php
 
+/**
+ * System configuration object
+ *
+ * @package Limny
+ * @author Hamid Samak <hamid@limny.org>
+ * @copyright 2009-2015 Limny
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class Config {
 	public $config;
 
+	/**
+	 * fetch config table and set values as property
+	 * @param object $db database connection object
+	 */
 	public function __construct($db = null) {
 		if (empty($db))
 			global $db;

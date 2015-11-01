2f746ced9aefe23a71d33719cab3cc688fe0c65e
Hamid Samak <hamidreza.samak@gmail.com>
2015-11-01 10:02:50 +0330

    Added header and comments

--- incs/permalink.class.php
+++ incs/permalink.class.php
@@ -1,12 +1,31 @@
 <?php
 
+/**
+ * Permalinks find/add/update/delete
+ *
+ * @package Limny
+ * @author Hamid Samak <hamid@limny.org>
+ * @copyright 2009-2015 Limny
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class Permalink {
+	// database connection
 	private $db;
 
+	/**
+	 * load registry
+	 * @param void
+	 */
 	public function __construct($registry) {
 		$this->db = $registry->db;
 	}
 
+	/**
+	 * check permalink exists or not
+	 * @param  string $permalink_str
+	 * @param  integer $exclude_id   exclude this id from existence ids
+	 * @return boolean
+	 */
 	public function permalink_exists($permalink_str, $exclude_id = null) {
 		if (is_array($permalink_str))
 			$permalink_str = implode('/', $permalink_str);
@@ -25,6 +44,11 @@ class Permalink {
 		return false;
 	}
 
+	/**
+	 * find permalink string by given page query parameter
+	 * @param  string         $query
+	 * @return string/boolean
+	 */
 	public function permalink_by_query($query) {
 		$result = $this->db->prepare('SELECT id, permalink FROM ' . DB_PRFX . 'permalinks WHERE query = ?');
 		$result->execute([$query]);
@@ -35,6 +59,11 @@ class Permalink {
 		return false;
 	}
 
+	/**
+	 * permalink generation pattern
+	 * @param  string $str
+	 * @return string     
+	 */
 	public function pattern($str) {
 		$str = str_replace(['-', '!', '.', '/', '\\', '"', '\''], '', $str);
 		$array = explode(' ', $str);
@@ -45,18 +74,41 @@ class Permalink {
 		return $str;
 	}
 
+	/**
+	 * insert new permalink
+	 * @param  string $query     page query parameter
+	 * @param  string $permalink permalink name
+	 * @return boolean
+	 */
 	public function add_permalink($query, $permalink) {
 		return $this->db->prepare('INSERT INTO ' . DB_PRFX . 'permalinks (query, permalink) VALUES (?, ?)')->execute([$query, $permalink]);
 	}
 
+	/**
+	 * update existence permalink by id or page query parameter
+	 * @param  integer/string $id_or_query       current permalink id or page query parameter
+	 * @param  string         $new_permalink_str
+	 * @return boolean
+	 */
 	public function update_permalink($id_or_query, $new_permalink_str) {
 		return $this->db->prepare('UPDATE ' . DB_PRFX . 'permalinks SET permalink = ? WHERE id = ? OR query = ?')->execute([$new_permalink_str, $id_or_query, $id_or_query]);
 	}
 
+	/**
+	 * delete permalink
+	 * @param  integer/string $id_or_query current permalink id or page query parameter
+	 * @return boolean
+	 */
 	public function permalink_remove($id_or_query) {
 		return $this->db->prepare('DELETE FROM ' . DB_PRFX . 'permalinks WHERE id = ? OR query = ?')->execute([$id_or_query, $id_or_query]);
 	}
 
+	/**
+	 * generate new permalink name & check for not being duplicate
+	 * @param  string  $title
+	 * @param  integer $id    exclude this id
+	 * @return string
+	 */
 	public function permalink_generate($title, $id = null) {
 		$permalink = $this->pattern($title);
 		$permalink_original = $permalink;

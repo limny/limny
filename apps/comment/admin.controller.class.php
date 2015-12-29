00c0baca2c9eb1dd9c8e8f40e88ade247ca9a30e
Hamid Samak <hamidreza.samak@gmail.com>
2015-12-29 12:07:03 +0330

    Added header and comments

--- apps/comment/admin.controller.class.php
+++ apps/comment/admin.controller.class.php
@@ -1,12 +1,31 @@
 <?php
 
+/**
+ * Comment administration controller
+ *
+ * @package Limny
+ * @author Hamid Samak <hamid@limny.org>
+ * @copyright 2009-2015 Limny
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class CommentAdminController extends Manage {
+	// page query parameter
 	public $q;
 
+	// page head
 	public $head;
 
+	// configuration values
 	public $config;
 
+	/**
+	 * call manage constructor
+	 * set configuration values
+	 * set model database connection and configuration
+	 * set administration script tag
+	 * @param  object $registry
+	 * @return void
+	 */
 	public function __construct($registry) {
 		parent::__construct($registry);
 
@@ -19,6 +38,10 @@ class CommentAdminController extends Manage {
 		$this->head = '<script type="text/javascript" src="' . BASE . '/apps/comment/js/admin.js"></script>';
 	}
 
+	/**
+	 * unapproved comments page
+	 * @return boolean
+	 */
 	public function unapproved() {
 		if (isset($_POST['action']))
 			$this->call_action($_POST['action'], $_POST);
@@ -40,6 +63,12 @@ class CommentAdminController extends Manage {
 		return true;
 	}
 
+	/**
+	 * call administration action
+	 * @param  string $action (approve / reply / edit / delete)
+	 * @param  array  $data
+	 * @return void
+	 */
 	private function call_action($action, $data) {
 		if (isset($data['id']) === false)
 			exit;
@@ -60,6 +89,12 @@ class CommentAdminController extends Manage {
 		exit;
 	}
 
+	/**
+	 * send email to parent comment email address
+	 * @param  integer  $comment_id
+	 * @param  boolean  $to_parent  send email to parent
+	 * @return boolean
+	 */
 	private function send_notification_mail($comment_id, $to_parent = true) {
 		$comment = CommentAdminModel::comment($comment_id);
 
@@ -82,6 +117,10 @@ class CommentAdminController extends Manage {
 		return true;
 	}
 
+	/**
+	 * all comments page
+	 * @return void
+	 */
 	public function all() {
 		$this->manage_title = COMMENT_ALL_COMMENTS;
 		$this->manage_table = 'comments';
@@ -153,10 +192,20 @@ class CommentAdminController extends Manage {
 		$this->content = $this->manage();
 	}
 
+	/**
+	 * get post title by id
+	 * @param  integer $post_id
+	 * @return string
+	 */
 	protected function post_title($post_id) {
 		return '<a href="' . url('post/' . $post_id) . '" target="_blank">' . $this->get_item($post_id, 'title', 'posts') . '</a>';
 	}
 
+	/**
+	 * get comment status in string with color
+	 * @param  integer $status
+	 * @return string
+	 */
 	protected function comment_status($status) {
 		if (empty($status))
 			return '<span class="text-red">' . COMMENT_UNAPPROVED . '</span>';

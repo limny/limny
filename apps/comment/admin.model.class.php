c93dad17ff8cd72d6844ee7c480aede1e5e9fe05
Hamid Samak <hamidreza.samak@gmail.com>
2015-12-29 18:54:35 +0330

    Added header and comments

--- apps/comment/admin.model.class.php
+++ apps/comment/admin.model.class.php
@@ -1,9 +1,24 @@
 <?php
 
+/**
+ * Comment administration model
+ *
+ * @package Limny
+ * @author Hamid Samak <hamid@limny.org>
+ * @copyright 2009-2015 Limny
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class CommentAdminModel {
+	// database connection
 	public static $db;
+
+	// configuration values
 	public static $config;
 
+	/**
+	 * get unapproved comments
+	 * @return array/boolean
+	 */
 	public static function unapproved() {
 		$result = self::$db->query('SELECT ' . DB_PRFX . 'comments.*, ' . DB_PRFX . 'posts.title AS post_title FROM ' . DB_PRFX . 'comments INNER JOIN ' . DB_PRFX . 'posts ON ' . DB_PRFX . 'posts.id = ' . DB_PRFX . 'comments.post WHERE (' . DB_PRFX . 'comments.approved IS NULL OR ' . DB_PRFX . 'comments.approved = 0) ORDER BY ' . DB_PRFX . 'comments.time');
 		$result->execute();
@@ -14,10 +29,21 @@ class CommentAdminModel {
 		return isset($comments) ? $comments : false;
 	}
 
+	/**
+	 * set comment as approved
+	 * @param  integer $comment_id
+	 * @return boolean
+	 */
 	public static function approve_comment($comment_id) {
 		return self::$db->prepare('UPDATE ' . DB_PRFX . 'comments SET approved = 1 WHERE id = ?')->execute([$comment_id]);
 	}
 
+	/**
+	 * insert reply comment
+	 * @param  integer $comment_id parent comment text
+	 * @param  string  $reply      comment text
+	 * @return boolean
+	 */
 	public static function submit_reply($comment_id, $reply) {
 		$parent = self::comment($comment_id);
 
@@ -37,6 +63,11 @@ class CommentAdminModel {
 		return true;
 	}
 
+	/**
+	 * get comment by id
+	 * @param  integer       $comment_id
+	 * @return array/boolean
+	 */
 	public static function comment($comment_id) {
 		$result = self::$db->prepare('SELECT * FROM ' . DB_PRFX . 'comments WHERE id = ?');
 		$result->execute([$comment_id]);
@@ -47,6 +78,11 @@ class CommentAdminModel {
 		return false;
 	}
 
+	/**
+	 * get name of user by user id
+	 * @param  integer        $user_id
+	 * @return string/boolean
+	 */
 	private static function profile_name($user_id) {
 		$result = self::$db->prepare('SELECT nick_name, first_name, last_name FROM ' . DB_PRFX . 'profiles WHERE user = ?');
 		$result->execute([$user_id]);
@@ -63,10 +99,21 @@ class CommentAdminModel {
 		return false;
 	}
 
+	/**
+	 * update comment text
+	 * @param  integer $comment_id
+	 * @param  string  $text
+	 * @return boolean
+	 */
 	public static function edit_reply($comment_id, $text) {
 		return self::$db->prepare('UPDATE ' . DB_PRFX . 'comments SET comment = ? WHERE id = ?')->execute([$text, $comment_id]);
 	}
 
+	/**
+	 * delete comment record
+	 * @param  integer $comment_id
+	 * @return void
+	 */
 	public static function delete_comment($comment_id) {
 		$db->prepare('DELETE FROM ' . DB_PRFX . 'comments WHERE id = ?')->execute([$comment_id]);
 

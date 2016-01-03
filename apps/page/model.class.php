bf105d88e555dbf9d6fc2551b283cd1b1e5af583
Hamid Samak <hamidreza.samak@gmail.com>
2016-01-03 19:11:43 +0330

    Added header & comments

--- apps/page/model.class.php
+++ apps/page/model.class.php
@@ -1,8 +1,22 @@
 <?php
 
+/**
+ * Page model
+ *
+ * @package Limny
+ * @author Hamid Samak <hamid@limny.org>
+ * @copyright 2009-2016 Limny
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class PageModel {
+	// database connection
 	public static $db;
 
+	/**
+	 * get page record by id
+	 * @param  integer       $id page id
+	 * @return array/boolean
+	 */
 	public static function page($id) {
 		$result = self::$db->prepare('SELECT * FROM ' . DB_PRFX . 'pages WHERE id = ?');
 		$result->execute([$id]);

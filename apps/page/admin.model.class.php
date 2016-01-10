486d7a7c4fbf3715dce441ec0622973cee03652d
Hamid Samak <hamidreza.samak@gmail.com>
2016-01-10 10:25:14 +0330

    Added header and comments

--- apps/page/admin.model.class.php
+++ apps/page/admin.model.class.php
@@ -1,8 +1,21 @@
 <?php
 
+/**
+ * Page administration model
+ *
+ * @package Limny
+ * @author Hamid Samak <hamid@limny.org>
+ * @copyright 2009-2016 Limny
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class PageAdminModel {
+	// database connection
 	public static $db;
 
+	/**
+	 * get last inserted page id
+	 * @return string
+	 */
 	public static function pages_last_id() {
 		$result = self::$db->query('SELECT id FROM ' . DB_PRFX . 'pages ORDER BY id DESC LIMIT 1');
 		$result->execute();

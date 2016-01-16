3e9a5d652e4afcac88a95f8c9bad8b4fa1401238
Hamid Samak <hamidreza.samak@gmail.com>
2016-01-16 15:55:52 +0330

    Added header and comments

--- apps/post/admin.model.class.php
+++ apps/post/admin.model.class.php
@@ -1,8 +1,21 @@
 <?php
 
+/**
+ * Post administration model
+ *
+ * @package Limny
+ * @author Hamid Samak <hamid@limny.org>
+ * @copyright 2009-2016 Limny
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class PostAdminModel {
+	// database connection
 	public static $db;
 
+	/**
+	 * get last post id
+	 * @return integer
+	 */
 	public static function posts_last_id() {
 		$result = self::$db->query('SELECT id FROM ' . DB_PRFX . 'posts ORDER BY id DESC LIMIT 1');
 		$result->execute();
@@ -12,6 +25,10 @@ class PostAdminModel {
 		return '0';
 	}
 
+	/**
+	 * get number of uncategorized posts
+	 * @return integer
+	 */
 	public static function num_uncategorized_posts() {
 		$result = self::$db->query('SELECT COUNT(id) AS count FROM ' . DB_PRFX . 'posts WHERE category IS NOT NULL OR category != \'\'');
 		$result->execute();
@@ -21,6 +38,10 @@ class PostAdminModel {
 		return $count['count'];
 	}
 
+	/**
+	 * get number of unpublished posts
+	 * @return integer
+	 */
 	public static function num_unpublished_posts() {
 		$result = self::$db->query('SELECT COUNT(id) AS count FROM ' . DB_PRFX . 'posts WHERE published IS NULL OR published != 1');
 		$result->execute();
@@ -30,6 +51,11 @@ class PostAdminModel {
 		return $count['count'];
 	}
 
+	/**
+	 * get category record by id
+	 * @param  integer       $cat_id category id
+	 * @return array/boolean
+	 */
 	public static function category($cat_id) {
 		$result = self::$db->prepare('SELECT * FROM ' . DB_PRFX . 'posts_cats WHERE id = ?');
 		$result->execute([$cat_id]);
